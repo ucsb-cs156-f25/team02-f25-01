16e53043b90310243d5975215d4617f2270310de
Nic Santos <nicolas.x.santos@gmail.com>
2025-11-03 21:57:48 -0800

    ns - created articlesFixtures

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,42 @@
+const articlesFixtures = {
+    oneArticle: [
+        {
+            id: 1,
+            title: "Article1",
+            url: "https://article1.com",
+            explanation: "Article 1 explanation.",
+            email: "article1@gmail.com",
+            dateAdded: "2025-11-03T19:45:00"
+        }
+    ],
+    threeArticles: [
+        {
+            id: 2,
+            title: "Article2",
+            url: "https://article2.com",
+            explanation: "Article 2 explanation.",
+            email: "article2@gmail.com",
+            dateAdded: "2025-11-03T19:45:00"
+        },
+        
+        {
+            id: 3,
+            title: "Article3",
+            url: "https://article3.com",
+            explanation: "Article 3 explanation.",
+            email: "article3@gmail.com",
+            dateAdded: "2025-11-03T19:45:00"
+        },
+        
+        {
+            id: 4,
+            title: "Article4",
+            url: "https://article4.com",
+            explanation: "Article 4 explanation.",
+            email: "article4@gmail.com",
+            dateAdded: "2025-11-03T19:45:00"
+        },
+    ],
+};
+
+export { articlesFixtures };
