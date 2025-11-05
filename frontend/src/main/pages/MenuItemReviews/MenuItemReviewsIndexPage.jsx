c46715bfa5e6b7fa3eae1e6fe0f3c2495edef80e
Noah Louie <noahzlouie@ucsb.edu>
2025-11-05 12:51:44 -0800

    nl - added index page for MenuItemReview

--- frontend/src/main/pages/MenuItemReviews/MenuItemReviewsIndexPage.jsx
+++ frontend/src/main/pages/MenuItemReviews/MenuItemReviewsIndexPage.jsx
@@ -1,17 +1,46 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import React from "react";
+import { useBackend } from "main/utils/useBackend";
 
-export default function MenuItemReviewIndexPage() {
-  // Stryker disable all : placeholder for future implementation
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import MenuItemReviewsTable from "main/components/MenuItemReviews/MenuItemReviewsTable";
+import { useCurrentUser, hasRole } from "main/utils/useCurrentUser";
+import { Button } from "react-bootstrap";
+
+export default function MenuItemReviewsIndexPage() {
+  const currentUser = useCurrentUser();
+
+  const {
+    data: menuItemReviews,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    ["/api/menuitemreview/all"],
+    { method: "GET", url: "/api/menuitemreview/all" },
+    // Stryker disable next-line all : don't test default value of empty list
+    [],
+  );
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+      return (
+        <Button
+          variant="primary"
+          href="/menuitemreviews/create"
+          style={{ float: "right" }}
+        >
+          Create MenuItemReview
+        </Button>
+      );
+    }
+  };
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p>
-          <a href="/menuitemreviews/create">Create</a>
-        </p>
-        <p>
-          <a href="/menuitemreviews/edit/1">Edit</a>
-        </p>
+        {createButton()}
+        <h1>MenuItemReviews</h1>
+        <MenuItemReviewsTable menuItemReviews={menuItemReviews} currentUser={currentUser} />
       </div>
     </BasicLayout>
   );
