2aa9c1f78bbc6e21c9fb6618379acad308b66cd8
Yi Lin <ylin985@ucsb.edu>
2025-11-05 09:17:47 -0800

    yl - add UCSBOrganizations table and test

--- frontend/src/fixtures/ucsbOrganizationsFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationsFixtures.js
@@ -10,20 +10,20 @@ const ucsbOrganizationsFixtures = {
 
   threeOrganizations: [
     {
-      orgCode: "ORG1",
+      orgCode: "1",
       orgTranslationShort: "AFR",
       orgTranslation: "Student Affairs",
       inactive: false,
     },
 
     {
-      orgCode: "ORG2",
+      orgCode: "2",
       orgTranslationShort: "FB",
       orgTranslation: "Freebirds",
       inactive: true,
     },
     {
-      orgCode: "ORG3",
+      orgCode: "3",
       orgTranslationShort: "IV",
       orgTranslation: "Isla Vista",
       inactive: false,
