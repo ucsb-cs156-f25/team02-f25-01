29fc1f008799836213cf26276e598c9c3d101db2
Nathan Ngo <nathan_ngo@ucsb.edu>
2025-11-06 23:25:25 -0800

    nn - fix organizations delete endpoint to use orgCode

--- frontend/src/main/utils/UCSBOrganizationsUtils.js
+++ frontend/src/main/utils/UCSBOrganizationsUtils.js
@@ -10,7 +10,7 @@ export function cellToAxiosParamsDelete(cell) {
     url: "/api/ucsborganizations",
     method: "DELETE",
     params: {
-      id: cell.row.original.orgCode,
+      orgCode: cell.row.original.orgCode,
     },
   };
 }
