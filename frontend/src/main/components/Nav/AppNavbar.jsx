d3ea3b59f6fba2fdaafa80962f5fb353c1afa33f
John Vu <jjvu@ucsb.edu>
2025-11-06 17:48:07 -0800

    fix

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -70,6 +70,7 @@ export default function AppNavbar({
                   </Nav.Link>
                   <Nav.Link as={Link} to="/helprequest">
                     Help Requests
+                  </Nav.Link>
                   <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitems">
                     UCSB Dining Commons Menu Items
                   </Nav.Link>
